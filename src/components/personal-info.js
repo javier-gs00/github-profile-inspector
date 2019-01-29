b12f9065feef502dd82b0a3afc92bcf7667eb986
Javier Gutierrez <javiergs1106@gmail.com>
2019-01-28 23:46:54 -0300

    Update deps and use the react-less-template v2

--- src/components/personal-info.js
+++ src/components/personal-info.js
@@ -1,76 +1,77 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import LoadingSpinner from './loading-spinner'
 import MenuContainer from './menu'
 // import RepositoriesList from './repos-list'
 
 const mapStateToProps = ({ users }) => ({
-    isLoading: users.isLoading,
-    avatarUrl: users.avatarUrl,
-    username: users.username,
-    realName: users.realName,
-    location: users.location,
-    htmlUrl: users.htmlUrl,
-    publicRepositories: users.publicRepositories,
-    followers: users.followers,
-    following: users.following,
-    bio: users.bio
+  isLoading: users.isLoading,
+  avatarUrl: users.avatarUrl,
+  username: users.username,
+  realName: users.realName,
+  location: users.location,
+  htmlUrl: users.htmlUrl,
+  publicRepositories: users.publicRepositories,
+  followers: users.followers,
+  following: users.following,
+  bio: users.bio
 })
 
 const PersonalInfo = props => {
-    return (props.isLoading
-        ? <LoadingSpinner repositoriesMessage={""}/>
-        : props.username === undefined
-        ? null
-        : props.username !== 'Not Found'
-        ?
-        <div className="info-container">
-            <div className="personal-info-container">
-                <div className="personal-info-image-container">
-                    <img
-                        src={props.avatarUrl}
-                        alt=""></img>
-                </div>
-                <div className="personal-info-data-container">
-                    <div className="personal-info-data">
-                        <div><FontAwesomeIcon icon="user-circle" /></div>
-                        <span>{props.username|| 'username'}</span>
-                    </div>
-                    <div className="personal-info-data">
-                        <div><FontAwesomeIcon icon="map-marker-alt" /></div>
-                        <span>{props.location || 'N/A'}</span>
-                    </div>
-                    <div className="personal-info-stats">
-                        <div className="stats-block">
-                            <span>{props.publicRepositories || 0}</span>
-                            <span>Public Repos</span>
-                        </div>
-                        <div className="stats-block">
-                            <span>{props.followers || 0}</span>
-                            <span>Followers</span>
-                        </div>
-                        <div className="stats-block">
-                            <span>{props.following || 0}</span>
-                            <span>Following</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="personal-bio">
-                <p>{props.realName || 'real name'}</p>
-                <p>{props.bio}</p>
-            </div>
-            <MenuContainer />
-            {/* <RepositoriesList /> */}
+  return props.isLoading ? (
+    <LoadingSpinner repositoriesMessage={''} />
+  ) : props.username === undefined ? null : props.username !== 'Not Found' ? (
+    <div className="info-container">
+      <div className="personal-info-container">
+        <div className="personal-info-image-container">
+          <img src={props.avatarUrl} alt="" />
         </div>
-        :
-        <div className="info-container">
-            <span className="user-not-found">
-                This user does not exists :S<br />Please try another one
-            </span>
+        <div className="personal-info-data-container">
+          <div className="personal-info-data">
+            <div>
+              <FontAwesomeIcon icon="user-circle" />
+            </div>
+            <span>{props.username || 'username'}</span>
+          </div>
+          <div className="personal-info-data">
+            <div>
+              <FontAwesomeIcon icon="map-marker-alt" />
+            </div>
+            <span>{props.location || 'N/A'}</span>
+          </div>
+          <div className="personal-info-stats">
+            <div className="stats-block">
+              <span>{props.publicRepositories || 0}</span>
+              <span>Public Repos</span>
+            </div>
+            <div className="stats-block">
+              <span>{props.followers || 0}</span>
+              <span>Followers</span>
+            </div>
+            <div className="stats-block">
+              <span>{props.following || 0}</span>
+              <span>Following</span>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="personal-bio">
+        <p>{props.realName || 'real name'}</p>
+        <p>{props.bio}</p>
+      </div>
+      <MenuContainer />
+      {/* <RepositoriesList /> */}
+    </div>
+  ) : (
+    <div className="info-container">
+      <span className="user-not-found">
+        This user does not exists :S
+        <br />
+        Please try another one
+      </span>
+    </div>
+  )
 }
 
 const PersonalInfoContainer = connect(mapStateToProps)(PersonalInfo)
--- src/components/search-form.js
+++ src/components/search-form.js
@@ -1,47 +1,50 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import { fetchUser } from '../actions/actions'
 
 const mapDispatchToProps = {
-    fetchUser: fetchUser
+  fetchUser: fetchUser
 }
 
 const SearchForm = props => {
-    const handleSubmit = e => {
-        e.preventDefault()
-        const query = document.getElementById('search-input').value
-        props.fetchUser(query)
-    }
+  const handleSubmit = e => {
+    e.preventDefault()
+    const query = document.getElementById('search-input').value
+    props.fetchUser(query)
+  }
 
-    return (
-        <div className="header-container">
-            <div className="align-header-containers">
-                <div className="appname-container">
-                    <span>GitHub</span><span>Profile Inspector</span>
-                </div>
-                <div className="search-form-container">
-                    <form 
-                        onSubmit={handleSubmit}
-                        className="search-form">
-                        <input
-                            id="search-input"
-                            type="text"
-                            name="search"
-                            className="search-input"
-                            placeholder="Who are you looking for?"/>
-                        <button className="search-button">
-                            <FontAwesomeIcon icon="search" />
-                        </button>
-                    </form>
-                </div>
-                <div className="header-options-container"></div>
-            </div>
+  return (
+    <div className="header-container">
+      <div className="align-header-containers">
+        <div className="appname-container">
+          <span>GitHub</span>
+          <span>Profile Inspector</span>
         </div>
-    )
+        <div className="search-form-container">
+          <form onSubmit={handleSubmit} className="search-form">
+            <input
+              id="search-input"
+              type="text"
+              name="search"
+              className="search-input"
+              placeholder="Who are you looking for?"
+            />
+            <button className="search-button">
+              <FontAwesomeIcon icon="search" />
+            </button>
+          </form>
+        </div>
+        <div className="header-options-container" />
+      </div>
+    </div>
+  )
 }
 
-const SearchFormContainer = connect(null, mapDispatchToProps)(SearchForm)
+const SearchFormContainer = connect(
+  null,
+  mapDispatchToProps
+)(SearchForm)
 
 export default SearchFormContainer
